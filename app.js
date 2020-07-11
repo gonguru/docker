bbf09b05dbdee80a0748bbafaa6bc4b3d04e5428
Bryan Góngora <b.gongora.utj@gmail.com>
2020-07-11 01:34:14 -0500

    added middleware

--- app.js
+++ app.js
@@ -3,13 +3,30 @@ const app = express();
 
 let objetoPedorro = {}
 
+function middlewareGlobal(req, res, next) {
+    console.log("Me llego una petición ahhhh!");
+    next();
+}
+
+app.use(middlewareGlobal);
+
+function functionMiddleware(req, res, next) {
+    let UA = req.get('User-Agent');
+    console.log(UA + ' Anda de chismoso');
+    next();
+}
+
 app.get('/', (req, res) => res.redirect('/obtener'));
 
-app.get('/obtener', (req, res) => {
+app.get('/obtener', functionMiddleware, (req, res) => {
     res.json(objetoPedorro)
 });
 
-app.get('/meter', (req, res) => {
+app.get('/meter',
+    [(req,res, next) => {
+        console.log(req.query);
+        next();
+    }],(req, res) => {
    const { query} = req;
    objetoPedorro = {
        ...objetoPedorro,
